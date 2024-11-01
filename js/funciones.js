44ed47e4dd013d20b050ed8074ce2dfa38aa0379
lluisespert <lluisespertc@gmail.com>
2024-11-01 11:14:35 +0000

    Instagram con bootstrap terminada

---
+++ js/funciones.js
@@ -0,0 +1,17 @@
+function imgHorizontales() {
+
+    window.open("../imghorizontal.html", "_self");
+
+}
+
+function imgVerticales() {
+
+    window.open("../imagenvertical.html", "_self");
+
+}
+
+function inicio() {
+
+    window.open("../index.html", "_self");
+
+}
